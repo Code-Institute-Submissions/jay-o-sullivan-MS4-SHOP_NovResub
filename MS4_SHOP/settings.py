53f3c405d313d9384dd69cb59c6fb076d6f9bcc2
jay-o-sullivan <jayosullivan25@gmail.com>
2021-11-18 01:50:02 +0000

    resolved secret key

--- MS4_SHOP/settings.py
+++ MS4_SHOP/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yf$(ltz*ry*5)73)d)ml2t_0e8*(%1kyxrv*!ye_gnu-l3rq+#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4-shop.herokuapp.com', 'localhost']
 
